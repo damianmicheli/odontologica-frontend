97e74e85bbae259e004d4cb57598ff9fc4eef77c
Damian Micheli <damianmicheli@gmail.com>
2022-07-09 11:24:26 -0300

    First commit

---
+++ js/paciente/post_paciente.js
@@ -0,0 +1,112 @@
+window.addEventListener("load", function () {
+  //Al cargar la pagina buscamos y obtenemos el formulario donde estarán
+  //los datos que el usuario cargará del nuevo paciente
+  const formulario = document.querySelector("#add_new_paciente");
+
+  document.querySelector("#fecha-ingreso").value = fechaActual();
+
+  //Ante un submit del formulario se ejecutará la siguiente funcion
+  formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    //creamos un JSON que tendrá los datos del nuevo paciente
+    const formData = {
+      nombre: document.querySelector("#nombre").value,
+      apellido: document.querySelector("#apellido").value,
+      dni: document.querySelector("#dni").value,
+      fechaIngreso: convertirFecha(
+        document.querySelector("#fecha-ingreso").value
+      ),
+      domicilio: {
+        calle: document.querySelector("#calle").value,
+        numero: document.querySelector("#numero").value,
+        localidad: document.querySelector("#localidad").value,
+        provincia: document.querySelector("#provincia").value,
+      },
+    };
+    //invocamos utilizando la función fetch la API pacientes con el método POST que guardará
+    //el paciente que enviaremos en formato JSON
+    const url = "/pacientes";
+    const settings = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+
+    let error = false;
+    fetch(url, settings)
+      .then((res) => {
+
+        if (!res.ok) {
+          console.log("Ocurrió un error de tipo " + res.status);
+          error = true;
+        } else {
+        
+        return res.json();
+        }
+      })
+      .then((data) => {
+        if (error) {
+          //Si hay algun error se muestra un mensaje diciendo que el paciente
+          //no se pudo guardar y se intente nuevamente
+          Swal.fire("Maldición!", "Hubo un problema con los datos!", "error");
+        } else {
+          //Si no hay ningun error se muestra un mensaje diciendo que el paciente
+          //se agrego bien
+
+          Swal.fire(
+            "Correcto!",
+            "Ya registramos a " + data.nombre + " " + data.apellido + " como nuevo paciente!",
+            "success"
+          );
+          //se dejan todos los campos vacíos por si se quiere ingresar otro paciente
+          resetUploadForm();
+        }
+      })
+      .catch((e) => {
+        //Si hay algun error se muestra un mensaje diciendo que el paciente
+        //no se pudo guardar y se intente nuevamente
+        Swal.fire(
+          "Maldición!",
+          "No pudimos registrar este paciente por este extraño error: " + e,
+          "error"
+        );
+      });
+  });
+
+  function fechaActual() {
+    let fecha = new Date();
+    const dd = String(fecha.getDate()).padStart(2, "0");
+    const mm = String(fecha.getMonth() + 1).padStart(2, "0"); //January is 0!
+    const yyyy = fecha.getFullYear();
+
+    fecha = dd + "/" + mm + "/" + yyyy;
+    return fecha;
+  }
+
+  function convertirFecha(fecha) {
+    return fecha.split("/").reverse().join("-");
+  }
+
+  function resetUploadForm() {
+    document.querySelector("#nombre").value = "";
+    document.querySelector("#apellido").value = "";
+    document.querySelector("#dni").value = "";
+    document.querySelector("#fecha-ingreso").value = fechaActual();
+    document.querySelector("#calle").value = "";
+    document.querySelector("#numero").value = "";
+    document.querySelector("#localidad").value = "";
+    document.querySelector("#provincia").value = "";
+  }
+
+  (function () {
+    let pathname = window.location.pathname;
+    if (pathname === "/") {
+      document.querySelector(".nav .nav-item a:first").addClass("active");
+    } else if (pathname == "/pacienteList.html") {
+      document.querySelector(".nav .nav-item a:last").addClass("active");
+    }
+  })();
+});
