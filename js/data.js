c3bab369cda5cc5107b68ba529e1ab68bc01eabb
Damian Micheli <damianmicheli@gmail.com>
2022-07-09 12:53:48 -0300

    probando exports

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 // Para Heroku:
-export const URL_BASE = 'https://odontologica-postgres.herokuapp.com';
+const URL_BASE = 'https://odontologica-postgres.herokuapp.com';
+exports.URL_BASE = URL_BASE
 
 // Para Localhost:
 //export const URL_BASE = "";
