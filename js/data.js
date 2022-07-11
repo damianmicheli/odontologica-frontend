c35556ce0ebf0168052df369777a426c03b0b952
Damian Micheli <damianmicheli@gmail.com>
2022-07-11 15:38:10 -0300

    Version final 2

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 //// Para Heroku:
- const URL_BASE = 'https://odontologica-postgres.herokuapp.com';
+const URL_BASE = 'https://odontologica-postgres.herokuapp.com';
 
 // Para Localhost:
 //const URL_BASE = "http://localhost:8080";
